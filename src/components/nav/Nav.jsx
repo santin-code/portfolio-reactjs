47467852dc2e536f5073cf22d5df199c0e183dd5
Arkalkeirosras <recargamarcas6@gmail.com>
2022-07-28 20:00:33 -0500

    add the components about, experience and services with respective media queries

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -5,15 +5,19 @@ import { FaUserAstronaut } from 'react-icons/fa'
 import { FaLaptopCode } from 'react-icons/fa'
 import { RiServiceLine } from 'react-icons/ri'
 import { TbMessageCode } from 'react-icons/tb'
+import { useState } from 'react'
 
 const Nav = () => {
+
+  const[active, setActive] = useState('#')
+
   return (
     <nav>
-      <a href="#home"> <SiHomeassistant /> </a>
-      <a href="#about"> <FaUserAstronaut /> </a>
-      <a href="#experience"> <FaLaptopCode /> </a>
-      <a href="#services"> <RiServiceLine /> </a>
-      <a href="#contact"> <TbMessageCode /> </a>
+      <a href="#home" onClick={()=> setActive('#home') } className={active === '#home' ? 'active' : ''}  > <SiHomeassistant /> </a>
+      <a href="#about" onClick={()=> setActive('#about') } className={active === '#about' ? 'active' : ''}  > <FaUserAstronaut /> </a>
+      <a href="#experience" onClick={()=> setActive('#experience') } className={active === '#experience' ? 'active' : ''}  > <FaLaptopCode /> </a>
+      <a href="#services"  onClick={()=> setActive('#services') } className={active === '#services' ? 'active' : ''}  > <RiServiceLine /> </a>
+      <a href="#contact"  onClick={()=> setActive('#contact') } className={active === '#contact' ? 'active' : ''}   > <TbMessageCode /> </a>
     </nav>
   )
 }
--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,9 +1,104 @@
 import React from 'react'
 import './services.css'
+import { BsCheck2All }  from 'react-icons/bs'
 
 const Services = () => {
   return (
-    <section id='services'>Services</section>
+    <section id='services'>
+      <h5>What I Offer</h5>
+      <h2>Services</h2>
+
+      <div className="container services__container">
+        <article className="service">
+          <div className="service__head">
+            <h3>UI/UX Design</h3>
+          </div>
+          <ul className='service__list'>
+            <li>
+              <BsCheck2All className='service__list-icon' />
+              <p>Lorem ipsum, dolor sit amet consectetur  elit.</p>  
+            </li>
+            <li>
+              <BsCheck2All className='service__list-icon' />
+              <p>Lorem ipsum, dolor sit amet consectetur  elit.</p>  
+            </li>
+            <li>
+              <BsCheck2All className='service__list-icon' />
+              <p>Lorem ipsum, dolor sit amet consectetur  elit.</p>  
+            </li>
+            <li>
+              <BsCheck2All className='service__list-icon' />
+              <p>Lorem ipsum, dolor sit amet consectetur  elit.</p>  
+            </li>
+            <li>
+              <BsCheck2All className='service__list-icon' />
+              <p>Lorem ipsum, dolor sit amet consectetur  elit.</p>  
+            </li>
+          </ul>
+        </article>
+        
+        <article className="service">
+          <div className="service__head">
+            <h3>Web Development</h3>
+          </div>
+          <ul className='service__list'>
+            <li>
+              <BsCheck2All className='service__list-icon' />
+              <p>Lorem ipsum, dolor sit amet consectetur  elit.</p>  
+            </li>
+            <li>
+              <BsCheck2All className='service__list-icon' />
+              <p>Lorem ipsum, dolor sit amet consectetur  elit.</p>  
+            </li>
+            <li>
+              <BsCheck2All className='service__list-icon' />
+              <p>Lorem ipsum, dolor sit amet consectetur  elit.</p>  
+            </li>
+            <li>
+              <BsCheck2All className='service__list-icon' />
+              <p>Lorem ipsum, dolor sit amet consectetur  elit.</p>  
+            </li>
+            <li>
+              <BsCheck2All className='service__list-icon' />
+              <p>Lorem ipsum, dolor sit amet consectetur  elit.</p>  
+            </li>
+            <li>
+              <BsCheck2All className='service__list-icon' />
+              <p>Lorem ipsum, dolor sit amet consectetur  elit.</p>  
+            </li>
+          </ul>
+        </article>
+
+        <article className="service">
+          <div className="service__head">
+            <h3>Content Creation</h3>
+          </div>
+          <ul className='service__list'>
+            <li>
+              <BsCheck2All className='service__list-icon' />
+              <p>Lorem ipsum, dolor sit amet consectetur  elit.</p>  
+            </li>
+            <li>
+              <BsCheck2All className='service__list-icon' />
+              <p>Lorem ipsum, dolor sit amet consectetur  elit.</p>  
+            </li>
+            <li>
+              <BsCheck2All className='service__list-icon' />
+              <p>Lorem ipsum, dolor sit amet consectetur  elit.</p>  
+            </li>
+            <li>
+              <BsCheck2All className='service__list-icon' />
+              <p>Lorem ipsum, dolor sit amet consectetur  elit.</p>  
+            </li>
+            <li>
+              <BsCheck2All className='service__list-icon' />
+              <p>Lorem ipsum, dolor sit amet consectetur  elit.</p>  
+            </li>            
+          </ul>
+        </article>
+
+      </div>
+    </section>
   )
 }
 
